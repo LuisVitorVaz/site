142db15d14b917066154c3da8b2b52436398b55c
LuisVitorVaz <105644153+LuisVitorVaz@users.noreply.github.com>
2024-08-03 17:37:07 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -5,48 +5,27 @@ var database = firebase.database();
 var dadosRef = database.ref("data");
 let blockElementsActive = false;
 
-dadosRef.once("value")
-  .then(function(snapshot) {
-    if (!snapshot.exists()) {
-      console.log("Nenhum dado encontrado.");
-    } else {
-      snapshot.forEach(function(childSnapshot) {
-        // Acessa o valor da temperatura
-        var temperature = childSnapshot.child("temperature").val();
-        console.log(temperature);
-        var dia = childSnapshot.child("data").val();
-        console.log(temperature);
-        var hora = childSnapshot.child("hora").val();
-        console.log(temperature);
-
-        // Chama a função para inserir os dados na tabela
-        inserir_dados(dia, hora, temperature);
-
-        const evento = new CustomEvent('atualizarDataHora', { detail: { dia, hora } });
-        document.dispatchEvent(evento);
-      });
-    }
-  })
-  .catch(function(error) {
-    console.error("Erro ao recuperar dados: " + error.message);
-  });
-
 // Escuta alterações contínuas no banco de dados
 dadosRef.on("value", function(snapshot) {
   // Limpa a tabela antes de adicionar os novos dados
   dadosList.innerHTML = "";
 
-  snapshot.forEach(function(childSnapshot) {
-    // Acessa o valor da temperatura
-    var temperature = childSnapshot.child("temperature").val();
+  if (!snapshot.exists()) {
+    console.log("Nenhum dado encontrado.");
+  } else {
+    snapshot.forEach(function(childSnapshot) {
+      // Acessa o valor da temperatura
+      var temperature = childSnapshot.child("temperatura").val();
+      var dia = childSnapshot.child("data").val();
+      var hora = childSnapshot.child("hora").val();
+      console.log(temperature);
+      // Chama a função para inserir os dados na tabela
+      inserir_dados(dia, hora, temperature);
 
-    // Valores fictícios para dia e hora
-    var dia = "01/01/2023";
-    var hora = "12:00";
-
-    // Chama a função para inserir os dados na tabela
-    inserir_dados(dia, hora, temperature);
-  });
+      const evento = new CustomEvent('atualizarDataHora', { detail: { dia, hora } });
+      document.dispatchEvent(evento);
+    });
+  }
 });
 
 function inserir_dados(dia, hora, temperature) {
